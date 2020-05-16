3ec64f4b5d289bab27b9fa1d6a460385a88f7bd0
Bruno Li <lisiuloong@gmail.com>
2020-05-16 19:38:26 -0400

    push players select values to an array

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const container = document.querySelector('.container');
 
-container.addEventListener('click', (event) => {
+const selectSquare = () => {
 	let target = event.target;
+
 	if (target.className === 'board') {
-		// console.log(target.dataset.value);
+		// console.dir(target);
 		human.selection.push(target.dataset.value);
 		target.textContent = 'O';
 		computerSelection();
-		// console.log(`human ${target.dataset.value}`);
-		// console.log(human.selection);
+		console.log(`human ${human.selection}`);
+		target.classList.add('selected');
+		console.dir(target);
 	}
-});
+};
 
 const computerSelection = () => {
 	let choices = document.querySelectorAll('.board');
 	let randomSelection = Math.floor(Math.random() * 9) + 1;
-	// randomSelection.toString();
 	[ ...choices ].filter((choice) => {
 		if (choice.dataset.value.includes(randomSelection)) {
+			console.log(choice);
 			choice.textContent = 'X';
+			ai.selection.push(choice.dataset.value);
+			console.log(`AI ${ai.selection}`);
+			choice.classList.add('selected');
 		}
 	});
-	// console.log(`computer ${randomSelection}`);
 };
 
 const players = (name) => {
@@ -29,7 +33,8 @@ const players = (name) => {
 	return { name, selection };
 };
 
-const human = players('human');
+container.addEventListener('click', selectSquare);
 
-console.log(human);
+const human = players('human');
+const computer = players('ai');
 const ai = players('ai');
